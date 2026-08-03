bdd84f8936ce40424f1ef22ae727ec0c801bd869
louis22giga <antonio.fernandes.luiz@escola.pr.gov.br>
2026-08-03 11:35:27 -0300

    2

---
+++ script.js
@@ -0,0 +1,29 @@
+// const botoes= document.querySelectorA11 ("button");
+
+botoes.forEach (function (botao){
+    let curtiu = false;
+    botao.addEventsListener("fui clicado");
+    function botaoClicado (){
+         console.log ("fui clicado");
+         let texto = botao.querySelector ("span");
+         if (curtiu === false){
+            texto.textContent++;
+            curtiu = true;
+        }else
+    }
+    
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
